58ccb3b7364f08813b6e9878561f0ad14418c861
kurt1 <kurt1@DESKTOP-F02OTH0>
2024-05-17 17:44:05 +0900

    기본환경설정

---
+++ src/main/java/com/jsh/home/MyProfileHome202405Application.java
@@ -0,0 +1,13 @@
+package com.jsh.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfileHome202405Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfileHome202405Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsh/home/MyProfileHome202405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsh.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfileHome202405ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
